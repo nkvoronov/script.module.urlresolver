0d8b0728be7b3258feeb55828db3ba25d490fdfe
Gujal00 <gujal@protonmail.com>
2018-01-18 19:14:10 +1300

    EnterVideo fix
    Fix Entervideo plugin.
    http://entervideo.net/watch/90c3ce519820f0d

--- lib/urlresolver/plugins/entervideo.py
+++ lib/urlresolver/plugins/entervideo.py
@@ -3,7 +3,7 @@
         t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
 
     urlresolver XBMC Addon
-    Copyright (C) 2011 t0mm0
+    Copyright (C) 2018 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,12 +18,29 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __generic_resolver__ import GenericResolver
+import re
+from lib import helpers
+from urlresolver import common
+from urlresolver.resolver import UrlResolver, ResolverError
 
-class EnterVideoResolver(GenericResolver):
+class EnterVideoResolver(UrlResolver):
     name = "entervideo"
     domains = ['entervideo.net']
     pattern = '(?://|\.)(entervideo\.net)/(?:watch/)?([0-9a-zA-Z]+)'
+    
+    def __init__(self):
+        self.net = common.Net()
 
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        response = self.net.http_GET(web_url, headers=headers)
+        html = response.content
+        if 'Not Found' in html:
+            raise ResolverError('File Removed')       
+        headers['Referer'] = web_url
+        stream_url = re.findall('source src="([^"]+)', html)[0]
+        return stream_url + helpers.append_headers(headers)
+        
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='http://entervideo.net/watch/{media_id}')
