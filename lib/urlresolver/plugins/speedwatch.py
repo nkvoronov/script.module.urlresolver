821e6e65cd60c0741593251749eda309c5dff379
TV ADDONS <30419907+tvaddonsco@users.noreply.github.com>
2019-10-17 13:48:06 -0700

    Version 5.0.40

--- lib/urlresolver/plugins/speedwatch.py
+++ lib/urlresolver/plugins/speedwatch.py
@@ -1,6 +1,6 @@
 '''
-    urlresolver XBMC Addon
-    Copyright (C) 2016 Gujal
+SpeedWatch.io urlresolver plugin
+Copyright (C) 2019 gujal
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,16 +15,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
-from lib import helpers
-from urlresolver.resolver import UrlResolver, ResolverError
 
-class HDvidResolver(UrlResolver):
-    name = 'HDvid'
-    domains = ['hdvid.tv']
-    pattern = '(?://|\.)(hdvid\.tv)/(?:embed-)?([0-9a-zA-Z]+)'
-    
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id), patterns=['''file:\s*["'](?P<url>[^"']+)''']).replace(' ', '%20')
+from __generic_resolver__ import GenericResolver
+
+class SpeedWatchResolver(GenericResolver):
+    name = "speedwatch"
+    domains = ["speedwatch.io"]
+    pattern = r'(?://|\.)(speedwatch\.io)/plyr/([0-9a-zA-Z]+)'
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id)
+        return self._default_get_url(host, media_id, template='https://www.{host}/plyr/{media_id}.html')
