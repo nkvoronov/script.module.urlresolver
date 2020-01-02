3bfa77fb79bd5d943d5ce7d31707cbd85bbaf6e3
TV ADDONS <30419907+tvaddonsco@users.noreply.github.com>
2020-01-02 12:14:54 -0500

    Version 5.0.48

--- lib/urlresolver/plugins/brighteon.py
+++ lib/urlresolver/plugins/brighteon.py
@@ -1,46 +1,52 @@
 """
-    Kodi urlresolver plugin
-    Copyright (C) 2019
+    Plugin for UrlResolver
+    Copyright (C) 2019 twilight0
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
+
+from lib import helpers
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
-from lib import helpers
 
 
-class MixdropResolver(UrlResolver):
-    name = "mixdrop"
-    domains = ["mixdrop.co", "mixdrop.to"]
-    pattern = r'(?://|\.)(mixdrop\.[ct]o)/(?:f|e)/(\w+)'
+class BrighteonResolver(UrlResolver):
+    name = "brighteon"
+    domains = ['brighteon.com']
+    pattern = r'(?://|\.)(brighteon\.com)/(?:embed)?/?([\w-]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
+
         web_url = self.get_url(host, media_id)
-        headers = {'Origin': 'https://{}'.format(host),
-                   'Referer': 'https://{}/'.format(host),
-                   'User-Agent': common.RAND_UA}
+
+        headers = {'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
 
-        if '(p,a,c,k,e,d)' in html:
-            html = helpers.get_packed_data(html)
-        r = re.search(r'vsr[^=]+=\s*"([^"]+)', html)
-        if r:
-            headers = {'User-Agent': common.RAND_UA}
-            return "https:" + r.group(1) + helpers.append_headers(headers)
+        try:
 
-        raise ResolverError("Video not found")
+            sources = helpers.scrape_sources(
+                html, patterns=[r'source src=[\'"](?P<url>.+?)[\'"].+?x-mpegURL']
+            )
+
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        except Exception:
+            raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
+
+        return self._default_get_url(host, media_id, template='https://www.{host}/embed/{media_id}')
--- lib/urlresolver/plugins/supervideo.py
+++ lib/urlresolver/plugins/supervideo.py
@@ -1,46 +1,45 @@
 """
-    Kodi urlresolver plugin
+    Plugin for UrlResolver
     Copyright (C) 2019
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import re
+from lib import helpers, jsunpack
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
-from lib import helpers
 
-
-class MixdropResolver(UrlResolver):
-    name = "mixdrop"
-    domains = ["mixdrop.co", "mixdrop.to"]
-    pattern = r'(?://|\.)(mixdrop\.[ct]o)/(?:f|e)/(\w+)'
+class supervideoResolver(UrlResolver):
+    name = "supervideo"
+    domains = ['supervideo.tv']
+    pattern = r'(?://|\.)(supervideo\.tv)/(?:embed-)?(\w+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
+
         web_url = self.get_url(host, media_id)
-        headers = {'Origin': 'https://{}'.format(host),
-                   'Referer': 'https://{}/'.format(host),
-                   'User-Agent': common.RAND_UA}
+        headers = {'User-Agent': common.RAND_UA, 'Referer': web_url}
         html = self.net.http_GET(web_url, headers=headers).content
 
-        if '(p,a,c,k,e,d)' in html:
-            html = helpers.get_packed_data(html)
-        r = re.search(r'vsr[^=]+=\s*"([^"]+)', html)
-        if r:
-            headers = {'User-Agent': common.RAND_UA}
-            return "https:" + r.group(1) + helpers.append_headers(headers)
+        html = jsunpack.unpack(re.search(r"(eval\(function.+\))", html, re.DOTALL).group(1))
 
-        raise ResolverError("Video not found")
+        try:
+            return re.search(r'''sources:\s*\[{file:\s*"(?P<url>[^"]+)''', html).group(1) + helpers.append_headers(headers)
+        except Exception:
+            raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
