64f41ae7f11a0a4b0b1d283efb0f3c5221603fa2
TV ADDONS <30419907+tvaddonsco@users.noreply.github.com>
2020-07-29 13:45:02 -0400

    Version 5.1.4 (thanks Gujal00)

--- lib/urlresolver/plugins/voesx.py
+++ lib/urlresolver/plugins/voesx.py
@@ -1,6 +1,6 @@
 """
     Plugin for URLResolver
-    Copyright (C) 2020 gujal
+    Copyright (C) 2020  gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,18 +15,14 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from urlresolver.plugins.lib import helpers
+
 from urlresolver.plugins.__generic_resolver__ import GenericResolver
 
 
-class NxloadResolver(GenericResolver):
-    name = "nxload"
-    domains = ["nxload.com"]
-    pattern = r'(?://|\.)(nxload\.com)/(?:v/|embed-)?([0-9a-zA-Z]+)'
-
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r"src:\s*'(?P<url>[^']+)"])
+class VoeResolver(GenericResolver):
+    name = "voe"
+    domains = ["voe.sx"]
+    pattern = r'(?://|\.)(voe\.sx)/(?:e/)?([0-9A-Za-z]+)'
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
