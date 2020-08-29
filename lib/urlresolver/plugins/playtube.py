510389f1ae6f221503bce5f6219d9b3253b56a3f
Nikolay Voronov <nkvoronov@gmail.com>
2020-08-29 22:27:29 +0300

    update

--- lib/urlresolver/plugins/playtube.py
+++ lib/urlresolver/plugins/playtube.py
@@ -1,4 +1,4 @@
-"""
+'''
     Plugin for URLResolver
     Copyright (C) 2020 gujal
 
@@ -14,19 +14,23 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+'''
+
 from urlresolver.plugins.lib import helpers
 from urlresolver.plugins.__generic_resolver__ import GenericResolver
 
 
-class NxloadResolver(GenericResolver):
-    name = "nxload"
-    domains = ["nxload.com"]
-    pattern = r'(?://|\.)(nxload\.com)/(?:v/|embed[-/])?([0-9a-zA-Z]+)'
+class PlayTubeResolver(GenericResolver):
+    name = "playtube"
+    domains = ["playtube.ws"]
+    pattern = r'(?://|\.)(playtube\.ws)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r"src:\s*'(?P<url>[^']+)"])
+                                     patterns=[r'''sources\s*:\s*\["(?P<url>[^"]+)''',
+                                               r'''file:\s*"(?P<url>[^"]+)'''],
+                                     generic_patterns=False,
+                                     result_blacklist=['dl', '.srt', '.vtt'])
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
--- lib/urlresolver/plugins/viduplayer.py
+++ lib/urlresolver/plugins/viduplayer.py
@@ -1,6 +1,9 @@
 """
+    OVERALL CREDIT TO:
+        t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
+
     Plugin for URLResolver
-    Copyright (C) 2020 gujal
+    Copyright (C) 2011 t0mm0
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,18 +18,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from urlresolver.plugins.lib import helpers
 from urlresolver.plugins.__generic_resolver__ import GenericResolver
 
 
-class NxloadResolver(GenericResolver):
-    name = "nxload"
-    domains = ["nxload.com"]
-    pattern = r'(?://|\.)(nxload\.com)/(?:v/|embed[-/])?([0-9a-zA-Z]+)'
-
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r"src:\s*'(?P<url>[^']+)"])
+class ViduPlayerResolver(GenericResolver):
+    name = "viduplayer"
+    domains = ['viduplayer.com']
+    pattern = r'(?://|\.)(viduplayer\.com)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
