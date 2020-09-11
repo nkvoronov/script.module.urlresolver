980eb368724410cc7e836d934698af61514bcf73
TV ADDONS <30419907+tvaddonsco@users.noreply.github.com>
2020-09-11 10:29:06 -0400

    Version 5.1.8 (thanks Gujal00)

--- lib/urlresolver/plugins/gounlimited.py
+++ lib/urlresolver/plugins/gounlimited.py
@@ -15,27 +15,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
-from urlresolver.resolver import UrlResolver, ResolverError
-from urlresolver import common
-from urlresolver.plugins.lib import jsunpack
-from urlresolver.plugins.lib import helpers
+from urlresolver.plugins.__generic_resolver__ import GenericResolver
 
-class GoUnlimitedResolver(UrlResolver):
+
+class GoUnlimitedResolver(GenericResolver):
     name = "gounlimited.to"
     domains = ['gounlimited.to']
     pattern = r'(?://|\.)(gounlimited\.to)/(?:embed-)?([0-9a-zA-Z]+)'
 
-    def get_media_url(self, host, media_id):
-	
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
-        html = self.net.http_GET(web_url, headers=headers).content
-        r = re.search(r"text/javascript'>(eval.*?)\s*</script>", html, re.DOTALL)
-        if r:
-            html = jsunpack.unpack(r.group(1))
-            sources = helpers.scrape_sources(html)
-            return helpers.pick_source(sources) + helpers.append_headers(headers)
-        raise ResolverError('Video cannot be located.')
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
